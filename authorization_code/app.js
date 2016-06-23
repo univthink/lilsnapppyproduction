fd9fd315b323040ccfbf476631f57d0eecad50be
Ben Barnett <benjamin@universallythinking.com>
2016-06-23 10:08:47 -0400

    Light Styling and Conditional Statement

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -18,8 +18,8 @@
  var request = require('request'); // "Request" library
  var querystring = require('querystring');
  var cookieParser = require('cookie-parser');
-//var ii = 1;
-if (self.location.href == "https://lilsnapppy.herokuapp.com/") {
+ ii = 1;
+if (ii === 1) {
  var client_id = '71d18cb9b32c480d951eed41512df8fc'; // Your client id
  var client_secret = '2e89cb3f772345279ae54fa417cc7457'; // Your secret
  var redirect_uri = 'https://lilsnapppy.herokuapp.com/callback/'; // Your redirect uri
