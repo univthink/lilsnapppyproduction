31d9e3d1f36e1b46bd23f386d4b53b3ead16b7f6
Ben Barnett <benjamin@universallythinking.com>
2016-06-25 16:32:12 -0400

    Voting Functionality

--- authorization_code/public/js/song.js
+++ authorization_code/public/js/song.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
                                     localStorage["myData"] = myData;
                                     Snapster = localStorage['Snapster'];
                                     $("#results").empty();
-                                    
+
                                     for (i = 0; i < myData.tracks.items.length; i++) {
                                         $('#results').append("<header class='songLink'>" + myData.tracks.items[i].artists[0].name + "<br />" + myData.tracks.items[i].name + "</header><br/>");
                                         $(".songLink").eq(i).attr("id", "songLink" + i);
@@ -86,7 +86,45 @@ $(document).ready(function () {
                                                                 $('#infoHeader').append("Upcoming Songs");
                                                                 $('#results').append("<header alt='0' class='songLinkCurrent'>" + currentPLData.items[i].track.artists[0].name + "<br />" + currentPLData.items[i].track.name + "</header><br/>");
                                                             }
+                                                            $.ajax( {
+                                                                type: "GET",
+                                                                url: "https://api.spotify.com/v1/users/" + userID + "/playlists/" + Snapster + "/tracks?limit=100&offset=" + localStorage["offsetNumber"],
+                                                                headers: { 'Authorization': 'Bearer ' + access_token },
+                                                                dataType: "json",
+                                                                data: "formdata",
+                                                                success: function (currentPLData) {
+                                                                    $('#results').empty();
+                                                                    for (i = 0; i < currentPLData.items.length; i++) {
+                                                                        $('#results').append("<header alt='" + i + "' style='color: gray;' class='songLinkClick' id='songLinkClick" + i + "'>" + currentPLData.items[i].track.artists[0].name + "<br />" + currentPLData.items[i].track.name + "</header><br/>");
+                                                                    }
+
+                                                                    for (i = 0; i < localStorage["totalSongs"] + 1; i++) {
+                                                                        if (i >= localStorage["currentTrack"] && localStorage["currentTrack"] > 3) {
+                                                                            document.getElementById("songLinkClick" + 4).style.color = "pink";
+                                                                            $(".songLinkClick:gt(4)").css("color", "white");
+                                                                        }
+                                                                        else if (i >= localStorage["currentTrack"] && localStorage["currentTrack"] == 0) {
+                                                                            document.getElementById("songLinkClick" + 0).style.color = "pink";
+                                                                            $(".songLinkClick:gt(0)").css("color", "white");
+                                                                        }
+                                                                        else if (i >= localStorage["currentTrack"] && localStorage["currentTrack"] == 1) {
+                                                                            document.getElementById("songLinkClick" + 1).style.color = "pink";
+                                                                            $(".songLinkClick:gt(1)").css("color", "white");
+                                                                        }
+                                                                        else if (i >= localStorage["currentTrack"] && localStorage["currentTrack"] == 2) {
+                                                                            document.getElementById("songLinkClick" + 2).style.color = "pink";
+                                                                            $(".songLinkClick:gt(2)").css("color", "white");
+                                                                        }
+                                                                        else if (i >= localStorage["currentTrack"] && localStorage["currentTrack"] == 3) {
+                                                                            document.getElementById("songLinkClick" + 3).style.color = "pink";
+                                                                            $(".songLinkClick:gt(3)").css("color", "white");
+                                                                        }
+
+                                                                    }
+                                                                  }
+                                                                });
                                                             $("#filename").val("");
+
                                                            location.reload();
                                                         }
                                                     });
